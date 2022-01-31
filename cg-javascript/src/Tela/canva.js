1c7dbe93039a8d4a0d6b8add2fe92e403a1f24e3
Guilherme Pacianotto <gpacianotto@gmail.com>
2022-01-31 13:04:52 -0300

    correção

--- cg-javascript/src/Tela/canva.js
+++ cg-javascript/src/Tela/canva.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 export default function Canva(){
-    const canvasElement = document.querySelector('#canvas-main')
+    const canvasElement = document.querySelector('#canvas-main');
+    
+        
     const [mouseDown, setMouseDown] = useState(false);
     const [scaled, setScaled] = useState(false);
     // const [MousePosition, setMousePosition] = useState({
@@ -8,14 +10,16 @@ export default function Canva(){
     //     top: 0
     // })
     console.log("canvas element: ", canvasElement);
+    
     function drawPixel(x, y, color) {
         let context = canvasElement.getContext('2d');
+
         var roundedX = Math.round(x);
         var roundedY = Math.round(y);
     
         context.beginPath();
         context.fillStyle = color || '#000';
-        context.fillRect(roundedX, roundedY, 1, 1);
+        context.fillRect(roundedX, roundedY, 4, 4);
         context.fill();
     }
     function handleMouseMove(ev){
@@ -37,11 +41,16 @@ export default function Canva(){
         //     canvasElement.height = window.innerHeight;
         //     setScaled(true)
         // }
-        
-    }, [mouseDown]);
+        let context = canvasElement.getContext('2d');
+
+        context.canvas.width  = window.innerWidth;
+        context.canvas.height = window.innerHeight;
+
+        setScaled(false);
+    }, []);
     return (
     <canvas
-    style={{height: "100%", width: "100%"}}
+    //style={{height: "100%", width: "100%"}}
     id="canvas-main"
     //onClick={() => {drawPixel((Math.floor(Math.random() * 100)), (Math.floor(Math.random() * 100)), 'black')}}
     onMouseDown={() => {setMouseDown(true)}}
